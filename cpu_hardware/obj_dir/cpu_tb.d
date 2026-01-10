0e0806df819c6d78ad9420c2e95933d0b3e4719f
juniornoodles <juniortchapmou@gmail.com>
2026-01-09 20:20:14 -0500

    Files for the cpu

--- cpu_hardware/obj_dir/cpu_tb.d
+++
@@ -1,6 +0,0 @@
-cpu_tb.o: ../cpu_tb.cpp Vcpu.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h \
- /usr/share/verilator/include/vltstd/svdpi.h Vcpu___024root.h
