7dc182593f3861124297a8edf32f054a8787f62b
juniornoodles <juniortchapmou@gmail.com>
2026-01-10 23:28:31 -0500

    Added an assembler to program the cpu easier

--- software/Branch.java
+++ software/Branch.java
@@ -1,3 +1,9 @@
+/*
+Written by Chris Junior Tchapmou
+January 10 2026
+*/
+
+//Just a simple class to hold branch labels and their line numbers
 package software;
 public class Branch{
 		public String name;
